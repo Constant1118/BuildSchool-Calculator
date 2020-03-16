2a973704f84d5d4a859cd86f16f8ccf20350bda6
子鵬 李 <hawk419383@gmail.com>
2020-03-16 15:15:48 +0800

    update

--- MathLirbry/Class1.cs
+++ MathLirbry/Class1.cs
@@ -8,5 +8,16 @@ namespace MathLirbry
 {
     public class Class1
     {
+        public  decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public decimal Less(decimal x, decimal y)
+        { return x - y; }
+        public decimal Multiply(decimal x , decimal y )
+        { return x * y; }
+        public decimal Except(decimal x, decimal y)
+        { return x % y; }
     }
 }
